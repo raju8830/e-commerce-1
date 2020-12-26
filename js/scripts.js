b6f38a0cbfa8a7bdff8d6a8015dec98c7a133d25
raju8830 <44889262+raju8830@users.noreply.github.com>
2020-12-26 12:15:41 +0600

    e-commars
    e-commars

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$("").click(function(){
+		$("").hide(2000);
+	});
+});
+$(document).ready(function(){
+	$("").hover(function(){
+		$("").hide(2000);
+	});
+});
+$(document).ready(function(){
+	$("").append(function(){
+		$("").hide(2000);
+	});
+});
+$(document).ready(function(){
+	$("").attr(function(){
+		$("").hide(2000);
+	});
+});
+$(document).ready(function(){
+	$("").animet(function(){
+		$("").hide(2000);
+	});
+});
